33fa01715f93ff4cc686bdd89b32d2283dace241
Abdullah030072 <abdullah030072@gmail.com>
2026-08-19 17:50:45 +0500

    Add project model

--- core/models.py
+++ core/models.py
@@ -41,3 +41,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='projects'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name

aa6b98fcd787060e84bffeed58f010f36de1f7e0
Abdullah030072 <abdullah030072@gmail.com>
2026-08-21 15:54:12 +0500

    Ticket 2: Update serializer for JWT authentication

--- core/serializers.py
+++ core/serializers.py
@@ -3,22 +3,21 @@ from rest_framework import serializers
 from .models import User, Profile, Project
 
 
-# User Serializer
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = [
-            "id",
-            "username",
-            "email",
-            "password",
+            'id',
+            'username',
+            'email',
+            'password',
         ]
-        read_only_fields = ["id"]
+        read_only_fields = ['id']
 
     def create(self, validated_data):
-        password = validated_data.pop("password")
+        password = validated_data.pop('password')
 
         user = User.objects.create_user(
             password=password,
@@ -28,34 +27,30 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
-# Profile Serializer
 class ProfileSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Profile
         fields = [
-            "id",
-            "user",
-            "profile_picture",
-            "role",
-            "contact_number",
+            'id',
+            'user',
+            'profile_picture',
+            'role',
+            'contact_number',
         ]
-        read_only_fields = ["id"]
+        read_only_fields = ['id', 'user']
 
 
-# Project Serializer
 class ProjectSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
 
     class Meta:
         model = Project
-        fields = "__all__"
+        fields = '__all__'
         read_only_fields = [
-            "id",
-            "created_by",
-            "created_at",
-            "updated_at",
+            'id',
+            'created_by',
+            'created_at',
+            'updated_at',
         ]
